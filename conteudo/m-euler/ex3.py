d6153714384fd83b56c7dd9566d196ea999437ce
Andrei <a.andreisilva@yahoo.com.br>
2016-09-07 14:19:39 -0300

    Execicio 3 da lista preparatória pra prova -> dy/dx = cos(x)

---
+++ conteudo/m-euler/ex3.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import math
+
+def main():
+    aprox = []
+    real = []
+    dx = 0.1
+    ant = x0 = 0
+
+    for i in range (0,100):
+        prox = ant + dx * math.cos(x0)
+        aprox.append(prox)
+        sen = math.sin(x0)
+        real.append(sen)
+        print('%.3f ---- %.3f'%(prox,sen))
+        x0+=dx
+
+    print(aprox)
+    print(real)
+
+    plt.plot(aprox, color = 'red', label = 'f(xn+dx) = f(xn-1) + dx*cos(xn-1)')
+    plt.plot(real, color = 'blue', label = 'g(x) = sen(x)')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend(loc = 'upper left')
+    plt.grid(True)
+    plt.show()
+    
+main()
