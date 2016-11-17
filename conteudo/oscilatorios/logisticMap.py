dfbb3763ea9397a7ca1645c88fb23f712c5bb9f2
Andrei <a.andreisilva@yahoo.com.br>
2016-11-17 20:26:47 -0200

    Capitulo 6 do livro - exercicio p2

--- conteudo/oscilatorios/logisticMap.py
+++ conteudo/oscilatorios/logisticMap.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 
 def main ():
     vetor = []
-    atual = 0.1
+    atual = 0.99
     prox = 0
-    r = 0.7
+    r = 0.76
 
     for i in range (1,100):
         prox = 4 * r * atual * (1 - atual)
         vetor.append(prox)
         atual = prox
-
     plt.plot (vetor, color = 'red')
     plt.show()
 
